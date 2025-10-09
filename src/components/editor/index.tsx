83bea711aa6c225de0ef27798ffc54eaee2e341e
Emmanuella <emmanuella.chukwu77@gmail.com>
2025-10-09 12:45:05 +0100

    fix: editor and preview scroll

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -4,6 +4,7 @@ import { resumeStore } from "@/store/resumeStore";
 import EditorForm from "./EditorForm";
 import Template from "../templates";
 import { initMockResume } from "@/lib/initResume";
+import { Download } from "lucide-react";
 
 initMockResume();
 
@@ -11,15 +12,22 @@ export default function ResumeEditor() {
   const snap = useSnapshot(resumeStore);
 
   return (
-    <div className="mt-24 w-full flex justify-center">
-      <div className="flex flex-row gap-x-8 w-[96%] h-[calc(100vh-10rem)]">
-        <div className="w-1/2 h-[80vh] shrink-0 pr-4 overflow-y-auto no-scrollbar">
+    <div className="fixed pt-24 inset-0 w-full flex justify-center">
+      <div className="flex flex-row gap-x-8 w-[96%] h-full">
+        <div className="w-1/2 shrink-0 pr-4 h-full overflow-y-auto no-scrollbar border">
           <EditorForm />
         </div>
         {/* preview */}
-        <div className="hidden lg:block lg:w-1/2 h-full overflow-y-auto no-scrollbar">
-          <h2 className="lg:text-xl font-bold">Resume Preview</h2>
-          <section className="">
+        <div className="hidden lg:flex flex-col lg:w-1/2 h-full border">
+          <div className="flex flex-row justify-between my-3 flex-shrink-0">
+            <h2 className="lg:text-xl font-bold">Resume Preview</h2>
+            <button
+              className="flex flex-row items-center gap-x-1 text-sm border border-slate-400 px-2 rounded-sm"
+            >
+              <Download size={15} /> Export as PDF
+            </button>
+          </div>
+          <section className="mb-3 overflow-y-auto no-scrollbar flex-1">
             <Template 
               header={snap.header}
               profileSummary={snap.profileSummary}
